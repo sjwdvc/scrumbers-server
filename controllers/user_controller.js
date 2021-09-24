1e111970ee410d9f79130fc2566d29103976347a
Rick <rlugtigheid77@gmail.com>
2021-09-24 12:52:50 +0200

    Added password hash validation

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,6 @@
 'use strict';
 // Imports
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
@@ -79,49 +80,54 @@ const deleteData = (req, res) => {
  */
 const login = (req, res) =>
 {
-  User.findById(req.params.id)
-    .then(data => {
-      if (!data) throw new Error('User not available');
-      // TODO:
-      // Comapre the password with password hash
-      if (/*req.body.password == data.password*/ true)
-      {
-        let key = null;
-        // Check if we have a jwt server key file
-        // If not create a new server key and put it in .jwtkey
-        if(fs.existsSync('.jwtkey'))
-          key = fs.readFileSync('.jwtkey');
-        else
-        {
-          key = require('crypto').createHash('md5').update(JSON.stringify(process.env)).digest("hex");
-          fs.writeFileSync('.jwtkey', key); // Safe the key to file
-        }
-        
-        // Create a json webtoken to use for api calls
-        let token = jwt.sign({
-          userName:  data.name,
-          userEmail: data.email
-        }, key);
-
-        // Send a response containing the token
-        res.status(200).json(
-        {
-          meta: {
-            count: 1
-          },
-          data: [
-            {
-              token: token
-            }
-          ]
-        }
-      );
-    }
-    else
-      res.status(401).json({ error: 'Password is incorrect' });
-  }).catch(err => {
-    res.status(500).json({ error: err.message });
-  });
+	console.log(req.body);
+	if (!req.body.password) return res.status(404).json({ error: 'Password Not found' });
+	User.findById(req.params.id)
+	.then(data => {
+		if (!data) res.status(404).json({ error: 'Not found' });
+		//if (!data) throw new Error('User not available');
+		// Validate the given password
+		bcrypt.compare(req.body.password, data.password).then(success =>
+		{
+			if (success)
+			{
+				let key = null;
+				// Check if we have a jwt server key file
+				// If not create a new server key and put it in .jwtkey
+				if(fs.existsSync('.jwtkey'))
+					key = fs.readFileSync('.jwtkey');
+				else
+				{
+					key = require('crypto').createHash('md5').update(JSON.stringify(process.env)).digest("hex");
+					fs.writeFileSync('.jwtkey', key); // Safe the key to file
+				}
+				
+				// Create a json webtoken to use for api calls
+				let token = jwt.sign({
+					userName:  data.name,
+					userEmail: data.email
+				}, key);
+		
+				// Send a response containing the token
+				res.status(200).json(
+					{
+						meta: {
+							count: 1
+						},
+						data: [
+							{
+								token: token
+							}
+						]
+					}
+				);
+			}
+			else
+				res.status(401).json({ error: 'Password is incorrect' });
+		});
+	}).catch(err => {
+		res.status(500).json({ error: err.message });
+	});
 }
  
 module.exports = {
