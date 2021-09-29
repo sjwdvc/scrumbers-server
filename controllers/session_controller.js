af2e354ef8c6a652c0b41cfcd959e7a0521159ab
Remynijsten <99012252@mydavinci.nl>
2021-09-29 12:31:06 +0200

    Reverted methodd

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -3,14 +3,11 @@
  * Checks wether you are logged in or not by the presence of the json webtoken
  */
 const check = (req, res) => {
-
-    res.json({ses: req})
-
-    // if(req.session.token === undefined) res.status(200).json({login: false})
-    // else
-    // {
-    //     res.status(200).json({login: true})
-    // }
+    if(req.session.token === undefined) res.status(200).json({login: false})
+    else
+    {
+        res.status(200).json({login: true})
+    }
 }
 
 const logout = (req, res) => {
