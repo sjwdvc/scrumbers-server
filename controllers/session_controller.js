43a7880a9b9622798c17b8f1909568ecc647b1ee
Remynijsten <99012252@mydavinci.nl>
2021-09-29 07:55:40 +0200

    Removed strict parameter types - possible 503

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,10 +1,6 @@
-const fs        = require('fs');
-
 /**
  * POST /session/check-login
  * Checks wether you are logged in or not by the presence of the json webtoken
- * @param {Request} req
- * @param {Response} res
  */
 const check = (req, res) => {
     if(req.session.token === undefined) res.status(200).json({login: false})
